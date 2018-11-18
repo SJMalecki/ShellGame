e1e9ee0aa063974af59c5ed1045e21de0e42bc66
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-18 21:57:11 +0100

    Added Game class that is responsible for displaying game-play on console by looping Player and Table classes method.

---
+++ src/pl/sjmprofil/Game.java
@@ -0,0 +1,35 @@
+package pl.sjmprofil;
+
+import java.util.Collections;
+
+public class Game {
+
+    public void game(String playerName){
+        int levelNumber = 1;
+        int boxes = 3;
+
+        Player player = new Player(playerName);
+        Table table = new Table("level "+ levelNumber, boxes);
+
+        while(true){
+
+            if(player.gameResult(table.choseBox())){
+                levelNumber++;
+                boxes++;
+                table = new Table("level "+ levelNumber, boxes);
+            }else{
+                //Collections.shuffle(table.getBoxList());
+            }
+
+            if(player.getChances() == 0){
+                System.out.println(playerName + " statistic: ");
+                System.out.println("Approaches " + player.getApproaches());
+                System.out.println("Points " + player.getPoints());
+                break;
+            }else{
+                System.out.println("Chances " + player.getChances());
+            }
+        }
+    }
+
+}
--- src/pl/sjmprofil/Main.java
+++ src/pl/sjmprofil/Main.java
@@ -3,7 +3,7 @@ package pl.sjmprofil;
 public class Main {
 
     public static void main(String[] args) {
-	    Table table = new Table("Normal Table", 3);
-	    table.choseBox();
+        Game game = new Game();
+        game.game("Ziomek");
     }
 }

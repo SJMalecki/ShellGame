147250999a81151bcb7193cb2865c0ad004e187a
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-19 21:58:55 +0100

    Bug fix, allows to continue playing after win

--- src/pl/sjmprofil/Game.java
+++ src/pl/sjmprofil/Game.java
@@ -1,17 +1,25 @@
 package pl.sjmprofil;
 
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Game {
+
+    Scanner scanner = new Scanner(System.in);
 
     public void game(String playerName){
         int levelNumber = 1;
         int boxes = 3;
+        boolean endInformationPopUp = false;
 
         Player player = new Player(playerName);
         Table table = new Table("level "+ levelNumber, boxes);
 
         while(true){
+            if(player.getPoints() == 3 && !endInformationPopUp){
+                System.out.println("\nCongratulations u won !\nyou can continue playing\npress enter to continue");
+                scanner.nextLine();
+                endInformationPopUp = true;
+            }
 
             if(player.gameResult(table.choseBox())){
                 levelNumber++;
@@ -19,11 +27,6 @@ public class Game {
                 table = new Table("level "+ levelNumber, boxes);
             }else{
                 //Collections.shuffle(table.getBoxList());
-            }
-
-            if(player.getPoints() == 5){
-                System.out.println("Congratulations u won !");
-                break;
             }
 
             if(player.getChances() == 0){
